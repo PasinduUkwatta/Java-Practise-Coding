3de07304a66e66e83bbb8ab105503538d5ee9e19
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-06-08 14:04:13 +0530

    Practise - Day 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
-import Pack2.NumbersAdd;
-
-
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        NumbersAdd n = new NumbersAdd();
-        n.addTwo(10,20);
     }
 }
