a52f2f9b33595cb6041b978a8a8f30d08b2a61c4
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-06-12 14:56:51 +0530

    Add files via upload

---
+++ Revolve.java
@@ -0,0 +1,13 @@
+public class Revolve {
+    public static void main(String[] args) {
+        Revolve earth = new Revolve();
+        earth.revolve();
+
+
+    }
+    void revolve(){
+        System.out.println("Revolve");
+    }
+
+
+}
