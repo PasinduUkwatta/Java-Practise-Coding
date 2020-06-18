c189e3fae34ab9ea53635a5be015dd22080db75b
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-06-18 14:53:59 +0530

    Practice -Day 9

---
+++ Conditions.java
@@ -0,0 +1,16 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int i =20;
+        if(i==25){
+            System.out.println("i is 25");
+
+        }else if(i==24){
+            System.out.println("i is 24");
+        }else {
+            System.out.println("i is not equal to 25 or 24");
+        }
+
+    }
+
+}
+
---
+++ SimpleCalSwitch.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SimpleCalSwitch {
+    public static void main(String[] args) {
+        //type obj =new Type()
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter no 1 :");
+        int num1 = scanner.nextInt();
+        System.out.println("The 1 number you enter :" + num1);
+
+        System.out.print("Enter no 2 :");
+        int num2 = scanner.nextInt();
+        System.out.println("The 2 number you enter :" + num2);
+
+        System.out.println("1 :ADD");
+        System.out.println("2 :SUBSTRACT");
+        System.out.println("3 :DEVIDE");
+        System.out.println("4 :MULTIPLY");
+        System.out.print("Enter Your Choice :");
+
+        int choice = scanner.nextInt();
+        System.out.println("Your Choice is  :" + choice);
+
+        switch (choice){
+
+            case 1: {
+                System.out.println(num1 + num2);
+                break;
+            }
+
+            case 2: {
+                System.out.println(num1 - num2);
+                break;
+            }
+
+            case 3: {
+                System.out.println(num1 / num2);
+                break;
+            }
+
+            case 4: {
+                System.out.println(num1 * num2);
+                break;
+            }
+
+            default:
+            {
+                System.out.println("invalid Entry");
+            }
+        }
+    }
+}
