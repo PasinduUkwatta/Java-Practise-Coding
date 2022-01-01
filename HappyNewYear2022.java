015ee9bf948280c48d1a10ce5cebc06ecfead12d
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-01-01 09:15:07 +0530

    first java code 2022

---
+++ HappyNewYear2022.java
@@ -0,0 +1,6 @@
+public class HappyNewYear2022 {
+    public static void main(String[] args) {
+        System.out.println("Happy New Year 2022");
+        System.out.println("All the best for new Year ");
+    }
+}
