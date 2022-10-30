eca5689a806fc7bbfd33ac44393a1fd9b7931958
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-10-30 12:59:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,106 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Car porsche =new Car();
-        Car holden =new Car();
-        BankAccount account =new BankAccount(132412,234535,"Pasindu","pasindu@gmail.com",0723452345);
-        VIPCustomer customer1 =new VIPCustomer("Deshan",200.00);
-        VIPCustomer customer2 =new VIPCustomer();
+	// write your code hereS
+        String parsingString ="2022";
+        System.out.println(parsingString);
 
-        porsche.setModel("Carrera");
+        int number =Integer.parseInt(parsingString);
+        System.out.println(number);
 
-        account.deposit(1000);
-        account.withdraw(500);
-        account.withdraw(1500);
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println("Enter Your Name :");
+//        String name =scanner.nextLine();
+//        System.out.println("Enter Your Year of Birth :");
+//
+//        boolean hasNextInt =scanner.hasNextInt();
+//
+//
+//        if(hasNextInt){
+//            int year =scanner.nextInt();
+//            int age =2022 -year;
+//            System.out.println("Your Name is :"+name);
+//            System.out.println(age);
+//            System.out.println(year);
+//
+//            if((year>=0) && (year<=100)){
+//                System.out.println("Your Age is :"+year);
+//
+//            }
+//            else {
+//                System.out.println("Invalid Age");
+//            }
+//        }
+//        else {
+//            System.out.println("Wrong Age Type");
+//        }
+//
+//        scanner.close();
+//        ReadingUserInput();
+        MinMaxSearch();
 
-        System.out.println(account);
-        System.out.println(customer1.getCreditLimit());
-        System.out.println(customer1.getName());
-        System.out.println(customer2.getName());
+    }
 
-        System.out.println(porsche.getModel());
+    public static void ReadingUserInput(){
+        int counter =0;
+        int sum =0;
+        Scanner scanner =new Scanner(System.in);
+
+
+
+        while (counter<10){
+            System.out.println("Enter Your Number "+(counter+1)+" :");
+
+            boolean hasNextInt =scanner.hasNextInt();
+            int number =scanner.nextInt();
+
+
+            if(hasNextInt){
+
+                sum=sum+number;
+                counter++;
+            }
+            else {
+                System.out.println("Please Enter Valid Number");
+            }
+
+        }
+        scanner.close();
+
+        System.out.println("Total of the Number :"+sum);
+
+    }
+
+    public static void MinMaxSearch(){
+        while (true){
+            Scanner scanner =new Scanner(System.in);
+            System.out.println("Enter Your Number");
+
+            int minValue =0;
+            int maxValue =0;
+
+            boolean hasNextInt = scanner.hasNextInt();
+            int enteredValue= scanner.nextInt();
+
+            if (hasNextInt){
+                if(enteredValue>maxValue ){
+                    maxValue=enteredValue;
+
+                }
+                if(enteredValue<minValue){
+                    maxValue=enteredValue;
+                }
+            }
+            else {
+                break;
+            }
+
+            System.out.println("Min Value :"+minValue);
+            System.out.println("Max Value :"+maxValue);
+        }
     }
 }
