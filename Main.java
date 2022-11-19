cb580e473124cc857516ba898ddec753c0461c28
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-11-19 12:04:51 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,55 +1,32 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
-    private static ArrayList<Album> albums =new ArrayList<Album>();
 
     public static void main(String[] args) {
+      int[] receivedValues= getIntegers();
+      printIntegers(receivedValues);
 	// write your code here
-
-        Album album =new Album("Strombringer","Deep Purple");
-        album.addSong("Strombringer",4.06);
-        album.addSong("A",4.26);
-        album.addSong("V",3.46);
-        album.addSong("C",2.56);
-        album.addSong("D",5.43);
-        album.addSong("T",2.46);
-        albums.add(album);
-
-        album=new Album("For those about","AC/DC");
-        album.addSong("A",2.60);
-        album.addSong("B",3.40);
-        album.addSong("C",3.24);
-        album.addSong("D",4.54);
-        album.addSong("E",2.47);
-        albums.add(album);
-
-
-        LinkedList<Song> playList =new LinkedList<Song>();
-        albums.get(0).addToPlayList("Strombringer",playList);
-        albums.get(0).addToPlayList("A",playList);
-        albums.get(0).addToPlayList("v",playList);
-        albums.get(0).addToPlayList("c",playList);
-        albums.get(0).addToPlayList(5,playList);
-        albums.get(1).addToPlayList(1,playList);
-        albums.get(2).addToPlayList(2,playList);
-        albums.get(3).addToPlayList(3,playList);
-
-        play(playList);
     }
 
-    private static void play(LinkedList<Song> playList){
-        ListIterator<Song>listIterator =playList.listIterator();
-        if(playList.size()==0){
-            System.out.println("No songs in playlist ");
-            return;
+    public static int[] getIntegers(){
+        Scanner scanner =new Scanner(System.in);
+        int [] array =new int[5];
+
+       
+
+        for(int i =0;i<5;i++){
+            System.out.println("Enter Your Number  "+i+" :");
+            int values =scanner.nextInt();
+            array[i]=values;
         }
-        else {
-            System.out.println("Now Playing "+listIterator.next().toString());
+        return array;
+    }
+
+    public static void printIntegers(int [] array){
+        for (int j : array) {
+            System.out.println(j);
         }
     }
 }
