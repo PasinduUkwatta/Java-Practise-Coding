148001382962e429bbb7ff9034935afc5d85c978
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-11-28 08:55:30 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        FootballPlayer joe =new FootballPlayer("Joe");
+        BaseBallPlayer pat =new BaseBallPlayer("Pat");
+        SoccerPlayer beckham =new SoccerPlayer("Beckham");
+
+        Team adelaideCrows =new Team("Adelide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
     }
 }
