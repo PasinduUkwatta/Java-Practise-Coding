2aedf4913b258b67ca649e397b95963278874a44
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-12-04 12:04:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+            String privateVar ="this is private main ()";
 
+            ScopeCheck scopeInstance =new ScopeCheck();
+        System.out.println("Scope instance privatevar is  :"+scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
 
-        FootballPlayer joe =new FootballPlayer("Joe");
-        BaseBallPlayer pat =new BaseBallPlayer("Pat");
-        SoccerPlayer beckham =new SoccerPlayer("Beckham");
+        scopeInstance.timesTwo();
 
-        Team adelaideCrows =new Team("Adelide Crows");
-        adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
-
-        System.out.println(adelaideCrows.numPlayers());
     }
 }
