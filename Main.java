948004546167859a3a83e47a1bcf0c59f74c2885
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-12-09 09:02:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Account timesAccount =new Account("Tim");
-        timesAccount.deposit(1000);
-        timesAccount.withdraw(500);
-        timesAccount.withdraw(-200);
-        timesAccount.deposit(-40);
-        timesAccount.calculateBalance();
 
-        System.out.println("Balnce on account is "+timesAccount.getBalance());
+        Theater theater =new Theater("Olympian",8,12);
+//        theater.getSeats();
+//        if(theater.reserveSeat("H11")){
+//            System.out.println("Please pay");
+//        }else {
+//            System.out.println("Sorry seat is taken !");
+//        }
 
+        List<Theater.Seat> seatCopy =new ArrayList<>(theater.seats);
+        printList(seatCopy);
+        seatCopy.get(1).reserved();
+        if(theater.reserveSeat("A02")){
+            System.out.println("Please pay for QA2");
+        }else{
+            System.out.println("Seat alreday booked");
+        }
+
+        public static void printList(List<Theater.Seat> list){
+            for(Theater.Seat seat:list){
+                System.out.println(" "+seat.getSeatNumber());
+            }
+
+            System.out.println();
+        }
     }
 }
