ecb904156649325f6272f0fde18d2110d31ae893
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2023-01-06 09:08:51 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import static jdk.internal.org.objectweb.asm.util.Printer.printList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Theater theater =new Theater("Olympian",8,12);
+        Theater theater = new Theater("Olympian", 8, 12);
 //        theater.getSeats();
 //        if(theater.reserveSeat("H11")){
 //            System.out.println("Please pay");
@@ -12,18 +14,18 @@ public class Main {
 //            System.out.println("Sorry seat is taken !");
 //        }
 
-        List<Theater.Seat> seatCopy =new ArrayList<>(theater.seats);
-        printList(seatCopy);
-        seatCopy.get(1).reserved();
-        if(theater.reserveSeat("A02")){
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+        public static void printList printList(seatCopy);
+        seatCopy.get(1).reserve();
+        if (theater.reserveSeat("A02")) {
             System.out.println("Please pay for QA2");
-        }else{
-            System.out.println("Seat alreday booked");
+        } else {
+            System.out.println("Seat already booked");
         }
 
-        public static void printList(List<Theater.Seat> list){
-            for(Theater.Seat seat:list){
-                System.out.println(" "+seat.getSeatNumber());
+        (List < Theater.Seat > list) {
+            for (Theater.Seat seat : list) {
+                System.out.println(" " + seat.getSeatNumber());
             }
 
             System.out.println();
