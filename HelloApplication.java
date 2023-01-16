7442194e0157b6fdcde14a36deb865b8cf8579b7
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2023-01-16 12:28:53 +0530

    Add files via upload

---
+++ HelloApplication.java
@@ -0,0 +1,38 @@
+package com.example.javafxapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+       GridPane root =new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setHgap(10);
+//        root.setVgap(10);
+//
+//        Label greeting =new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman",FontWeight.BOLD,70));
+//        root.getChildren().add(greeting);
+
+        stage.setTitle("Hello JavaFX!");
+        stage.setScene(new Scene(root,800,275));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
