db6d166d152ddc9d3d6ec09a3c148866f0a20bcb
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2023-04-11 12:43:53 +0530

    Add files via upload

--- ThreadColor.java
+++ ThreadColor.java
@@ -1,7 +1,4 @@
-/**
- * Created by timbuchalka on 25/05/2016.
- */
-public class ThreadColor {
+ public class ThreadColor {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
@@ -9,4 +6,5 @@ public class ThreadColor {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-}
+};
+
