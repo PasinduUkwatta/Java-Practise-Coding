9424be7c574ff2fa615aa2954fea8ec63ee31fa3
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2023-04-11 12:13:49 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class Main {
+    public static final String EOF ="EOF";
     public static void main(String[] args) {
-        Countdown countdown = new Countdown();
-        CountdownThread t1 = new CountdownThread(countdown);
-        t1.setName("Thread 1");
+        List<String> buffer =new ArrayList<String>();
+        MyProducer producer =new MyProducer(buffer,ThreadColor.ANSI_CYAN);
+        MyConsumer consumer1 =new MyConsumer(buffer,ThreadColor.ANSI_PURPLE);
+        MyConsumer consumer2 =new MyConsumer(buffer,ThreadColor.ANSI_GREEN);
 
-        CountdownThread t2 = new CountdownThread(countdown);
-        t2.setName("Thread 2");
-
-        t1.start();
-        t2.start();
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
     }
 }
 
-class Countdown {
+class MyProducer implements Runnable{
 
-    private int i;
+    private List<String> buffer;
+    private String color;
 
-    public void doCountdown() {
-        String color;
-        switch (Thread.currentThread().getName()) {
-            case "Thread 1":
-                color = ThreadColor.ANSI_CYAN;
-                break;
-            case "Thread 2":
-                color = ThreadColor.ANSI_PURPLE;
-                break;
-            default:
-                color = ThreadColor.ANSI_GREEN;
-        }
-
-        for (i = 10; i > 0; i--) {
-            System.out.println(color + Thread.currentThread().getName() + ":i =" + i);
-        }
-    }
-}
-
-class CountdownThread extends Thread {
-    private Countdown threadCountdown;
-
-
-    public CountdownThread(Countdown countdown) {
-        threadCountdown = countdown;
+    public MyProducer(List<String> buffer,String color) {
+        this.buffer = buffer;
+        this.color=color;
     }
 
     @Override
     public void run() {
-        threadCountdown.doCountdown();
+        Random random =new Random();
+        String [] nums ={"1","2","3","4","5"};
+        for (String num:nums){
+            try {
+                System.out.println(color+"Adding ..."+num);
+                synchronized (buffer){
+                    buffer.add(num);
+
+                }
+
+                Thread.sleep(random.nextInt(1000));
+
+            }catch (InterruptedException e){
+                System.out.println("Producer was interuppted");
+            }
+        }
+
+        System.out.println(color +" Adding EOF and exit..");
+       synchronized (buffer){
+           buffer.add("EOF");
+
+       }
+    }
+}
+
+class MyConsumer implements Runnable{
+
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer,String color) {
+        this.buffer = buffer;
+        this.color=color;
+    }
+
+    @Override
+    public void run() {
+    while (true){
+        synchronized (buffer){
+            if(buffer.isEmpty()){
+                continue;
+            }
+            if(buffer.get(0).equals("EOF")){
+                System.out.println(color +"Exiting");
+                break;
+            }else {
+                System.out.println(color+ "Removed " +buffer.remove(0));
+            }
+        }
+
+    }
     }
 }
