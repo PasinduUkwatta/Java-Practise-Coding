dda277fa3295c1f140749a03d6e62d1151767427
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2024-01-08 09:40:43 +0530

    Add files via upload

--- StudentCollectionRunner.java
+++ StudentCollectionRunner.java
@@ -2,9 +2,19 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+class AsendingStudentComaprator implements Comparator<StudentCollection>{
+    @Override
+    public int compare(StudentCollection student1, StudentCollection student2) {
+
+        return Integer.compare(student1.getId(),student2.getId());
+    }
+}
+
 public class StudentCollectionRunner {
+    
     public static void main(String[] args) {
         List<StudentCollection> students = List.of(new StudentCollection(1,"Pasindu"),new StudentCollection(2,"Thiwnaka"));
         System.out.println(students);
