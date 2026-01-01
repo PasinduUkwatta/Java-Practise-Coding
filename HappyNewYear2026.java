17b3b7308428b3487bf51619ef32c953d75f8f06
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2026-01-01 07:04:38 +0530

    Add files via upload

---
+++ HappyNewYear2026.java
@@ -0,0 +1,7 @@
+package com.aspectoriented.aspectoriented.dao;
+
+public class HappyNewYear2026 {
+    public static void main(String[] args) {
+        System.out.println("Happy New Year 2026");
+    }
+}
