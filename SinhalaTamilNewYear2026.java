e83581585ebad163848ef1ac2b1c66325f2a1810
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2026-04-14 12:16:32 +0530

    2026

---
+++ SinhalaTamilNewYear2026.java
@@ -0,0 +1,7 @@
+package com.aspectoriented.aspectoriented.dao;
+
+public class SinhalaTamilNewYear2026 {
+    public static void main(String[] args) {
+        System.out.println("Happy Sinhala Tamil New Year 2026");
+    }
+}
